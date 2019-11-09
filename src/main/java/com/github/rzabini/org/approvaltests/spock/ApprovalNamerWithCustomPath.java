9500a89e9f4d39f33694615ce95aa0a4d49b0dad
rzabini <romano.zabini@gmail.com>
2019-11-09 11:04:33 +0100

    fix: checkstyle

--- src/main/java/com/github/rzabini/org/approvaltests/spock/ApprovalNamerWithCustomPath.java
+++ src/main/java/com/github/rzabini/org/approvaltests/spock/ApprovalNamerWithCustomPath.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
  */
 public class ApprovalNamerWithCustomPath implements ApprovalNamer {
 
+    private static final String TEST_DIRECTORY_NAME = "test";
     private final ApprovalNamer approvalNamer;
 
     ApprovalNamerWithCustomPath(final ApprovalNamer approvalNamer) {
@@ -23,7 +24,8 @@ public class ApprovalNamerWithCustomPath implements ApprovalNamer {
 
     @Override
     public String getSourceFilePath() {
-        return approvalNamer.getSourceFilePath().replaceAll("test"+ Matcher.quoteReplacement(File.separator) + "(groovy|java)",
-                "test" + File.separator + "resources");
+        return approvalNamer.getSourceFilePath().replaceAll(
+                TEST_DIRECTORY_NAME + Matcher.quoteReplacement(File.separator) + "(groovy|java)",
+                TEST_DIRECTORY_NAME + File.separator + "resources");
     }
 }
